29a904519b7e89ba6a7040382b8790251a64eb90
Jason Barnett <jason@thebarnetts.ws>
2016-11-29 15:18:41 -0600

    adds interaction

--- main.js
+++ main.js
@@ -3,20 +3,38 @@ var app = express()
 var port = 4000
 app.listen(port)
 
+var inquirer = require('inquirer')
 
-drawBoard()
+greeting()
 
 
 
-function makeMove(){}
+var board = [ '\n', '1', '2', '3', '\n', '4', '5', '6', '\n', '7', '8', '9', '\n']
+
+
+function greeting(){
+  inquirer.prompt({
+    name: 'TicTacToeTime',
+    message: 'Ready to Play?',
+    validate: function(val){
+      if (val.toLowerCase() === 'y'){
+        drawBoard()
+        makeMove()
+      } 
+    }
+  })
+}
+
+function makeMove(){
+  inquirer.prompt({
+    name: 'PlayerMove',
+    message: 'Enter Your Move: ,
+    validate: function(val){
+       
+    }
+  })
+}
 
 function drawBoard(){
-  var board = [
-  ' 1 | 2 | 3 \n',
-  '---|---|---\n',
-  ' 4 | 5 | 6 \n',
-  '---|---|---\n',
-  ' 7 | 8 | 9 \n'
-  ]
-  console.log(board.join(''))
+  console.log(board.join('  '))
 }
